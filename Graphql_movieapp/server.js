c9b731f615894fe294bce9610ce485cc4aebab9c
DHkimgit <dukekim001@naver.com>
2022-09-02 03:42:07 +0000

    09021242

--- Graphql_movieapp/server.js
+++ Graphql_movieapp/server.js
@@ -1,14 +1,24 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const typeDef = `
-    type Query{
+const typeDefs = `
+    type User {
+        id: ID,
+        username: String
+    }
+    type Tweet {
+        id: ID,
         text: String
-        
+        author: User
+    }
+    type Query{
+        allTweets: [Tweet],
+        tweet(id: ID): Tweet
     }
 `
 
-const server = new ApolloServer({});
+const server = new ApolloServer({typeDefs});
 
 server.listen().then(({url}) => {
     console.log(`Running on ${url}`);
 })
+
