15a2fba71bf2a4ad2a53b0972b63d7e95b224a39
DHkimgit <dukekim001@naver.com>
2022-09-11 11:20:39 +0000

    09112020

--- Mongodb_graphql_tutorial/graphql/resolvers/index.js
+++ Mongodb_graphql_tutorial/graphql/resolvers/index.js
@@ -1,17 +1,22 @@
 const Content = require('../../database/models/content');
 const Person = require('../../database/models/persons');
+const User1 = require('../../database/models/user1');
+const Booking1 = require('../../database/models/booking1');
+const { startSession } = require('mongoose')
+
 const resolvers = {
   Query: {
     async people(_, args){
         try {
             console.log(args.id);
-            const people = await Person.find({"_id": `${args.id}`}).exec();
+            const people = await Person.findOne({ _id: { $in: args.id } });
             console.log(people)
             return people;
         } catch (err) {
             console.log(err);
             throw err;
         }
+        //{"_id": `${args.id}`}
     },
     async allPeople(_, args){
       try {
@@ -30,7 +35,25 @@ const resolvers = {
             console.log(err);
             throw err;
         }
-    }
+    },
+    async user1s(_, args){
+      try {
+        const user1s = await User1.find();
+        return user1s;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    },
+    async booking1s(_, args) {
+      try {
+        const booking1s = await Booking1.find();
+        return booking1s;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    },
   },
   Person: {
     _id(_, args) {
@@ -42,6 +65,24 @@ const resolvers = {
     async friends(_,args){
       const friends = await Person.find({ _id: { $in: _.friendIds } })
       return friends
+    },
+  },
+  User1: {
+    async booking1s(_,args){
+      const booking1s = await Booking1.find({ _id: { $in: _.booking1Ids } })
+      return booking1s
+    },
+  },
+  Booking1: {
+    async user1(_, args) {
+      try {
+        const user1 = await User1.findOne({ booking1Ids: { $in: _._id } })
+        console.log(user1);
+        return user1;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     },
   },
   Content: {
@@ -60,17 +101,45 @@ const resolvers = {
   },
   Mutation: {
     async createContent(_, args) {
-        try {
-            const content = new Content({
-            ...args.contentInput
+      try {
+        const content = new Content({
+          ...args.contentInput
         })
         const result = await content.save();
         return result;
-        } catch (err) {
+      } catch (err) {
         console.log(err);
         throw err;
-    }
-    }
+      }
+    },
+    async createUser1(_, args){
+      try {
+        const user1 = new User1({
+          ...args.user1Input
+        })
+        const result = await user1.save();
+        return result;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    },
+    async createBooking1(_, args) {
+      try {
+        const booking1 = new Booking1({
+          name: args.booking1Input.name
+        })
+        const result = await booking1.save();
+        await User1.findByIdAndUpdate(args.booking1Input.user1Id,
+          { $push: { booking1Ids: result._id } },
+          { useFindAndModify: false }
+        );
+        return result;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    }    
   }
 };
 
--- Mongodb_graphql_tutorial/graphql/schema/index.js
+++ Mongodb_graphql_tutorial/graphql/schema/index.js
@@ -3,13 +3,34 @@ const typeDefs = gql`
   type Query {
     allPeople:[Person]
     contents  : [Content]
-    people(id: String!): Person
+    people(id: String!): Person!
+    user1s: [User1]
+    booking1s: [Booking1]
+  }
+  type Booking1{
+    _id: ID
+    name: String
+    user1: User1
+    createdAt: String
+  }
+  input Booking1Input{
+    name: String
+    user1Id: String
+  }
+  type User1{
+    _id: ID
+    name: String
+    booking1s: [Booking1]
+    createdAt: String
+  }
+  input User1Input{
+    name: String
   }
   type Content {
-    _id: ID
-    title: String
-    content: String
-    createdAt: String
+    _id: ID!
+    title: String!
+    content: String!
+    createdAt: String!
   }
   type Person{
     _id: ID
@@ -21,7 +42,9 @@ const typeDefs = gql`
     content: String
   }
   type Mutation{
-    createContent(contentInput: ContentInput): [Content]!
+    createContent(contentInput: ContentInput): Content!
+    createUser1(user1Input: User1Input): User1!
+    createBooking1(booking1Input: Booking1Input): Booking1!
   }
 `;
 
